a660b3739cae19fe56c34cbc5fac39f2eb126d93
Jorge Oliveira <jorgeandreoliveira@gmail.com>
2019-11-04 22:18:32 -0300

    GitHub, Storage e Navigation

---
+++ src/pages/User/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+export default class User extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  });
+
+  render() {
+    return <View/>;
+  }
+
+}
